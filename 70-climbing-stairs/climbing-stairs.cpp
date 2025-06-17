a50f235e43b97549b85bde15209e9b2c870ac17c
Harshavardan Bandari <112824664+harshakun@users.noreply.github.com>
2025-06-17 18:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (35.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+int check(int n ,vector<int>& dp)
+{
+    if(n==0) return 1;
+    if(n<0) return 0;
+    if(dp[n]!=-1) return dp[n];
+    int a = check(n-1,dp);
+    int b = check(n-2,dp);
+    dp[n]=a+b;
+    return a+b;
 
-    int check (int n ,vector<int> & dp)
-    {
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-
-        int x = check(n-1,dp)+check(n-2,dp);
-        dp[n]=x;
-        return x;
-    }
+} 
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        int c = check(n,dp);
-        return c;
+        int x = check( n , dp);
+        return x;
 
         
     }
