7fb504d42ea6029c54fabc2d8e0c00f912d7b02d
Harshavardan Bandari <112824664+harshakun@users.noreply.github.com>
2025-07-18 18:03:36 +0530

    Time: 490 ms (15.41%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,2 +1,5 @@
-/* Write your T-SQL query statement below */
-select score , dense_rank() over(order by score desc) as rank from Scores
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM Scores
+ORDER BY score DESC
