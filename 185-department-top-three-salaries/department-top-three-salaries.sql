bd442f03fdbddf797626d9ee60855c97d7011b15
Harshavardan Bandari <112824664+harshakun@users.noreply.github.com>
2025-07-19 10:19:41 +0530

    Time: 625 ms (16.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+select b.name as Department , a.name as Employee , a.Salary  as Salary 
+from (select *, dense_rank() over(partition by departmentId  order by salary desc) as rk  from employee) a
+join Department b on a.departmentId = b.id where a.rk <=3
