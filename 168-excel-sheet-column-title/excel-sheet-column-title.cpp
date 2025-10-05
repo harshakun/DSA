9e64d84f376055318cd51365d0b891155227df8e
Harshavardan Bandari <112824664+harshakun@users.noreply.github.com>
2025-10-05 15:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (21.76%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans ="";
+        while(n>0)
+        {
+            int rem = n%26;
+            if(rem ==0 )
+            {
+                ans +='Z';
+                n=n/26 -1;
+            }
+            else 
+            {
+                ans += rem-1 + 'A';
+                n=n/26;
+            }
+
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
